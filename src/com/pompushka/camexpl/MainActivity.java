7d8de7b886afcfbe0d1b8e2f5a693309ceee475b
XoXoTyXa <ChupashevAV@Gmail.com>
2015-04-06 20:24:00 +0600

    Ver_0.01
    Cam access sample and some experiments with.

---
+++ src/com/pompushka/camexpl/MainActivity.java
@@ -0,0 +1,100 @@
+package com.pompushka.camexpl;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	private TextView tv;
+	private Camera myCam;
+	private CameraPreview mPreview;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        
+        tv = (TextView) findViewById(R.id.textView2);
+        /*
+        if (checkCameraHardware(getApplicationContext())) 
+        	tv.setText("There are "+ String.valueOf(Camera.getNumberOfCameras()) + " cameras");
+        else tv.setText("There is no cam");
+        
+        CameraInfo cameraInfo = null;
+        Camera.getCameraInfo(1, cameraInfo);
+        if (cameraInfo.facing == cameraInfo.CAMERA_FACING_BACK){
+        	tv.setText("Камера 1 задняя");
+        	myCam = getCameraInstance();
+        }
+
+        myCam.release();*/
+        
+        // Create an instance of Camera
+        myCam = getCameraInstance(1);
+
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, myCam);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+    }
+    
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // The activity is about to become visible.
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // The activity has become visible (it is now "resumed").
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Another activity is taking focus (this activity is about to be "paused").
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // The activity is no longer visible (it is now "stopped")
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // The activity is about to be destroyed.
+        myCam.release();
+    }
+    
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+    
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(int camera){
+        Camera c = null;
+        try {
+            c = Camera.open(camera); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+}
